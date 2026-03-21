969010f7b1ac51ab26d1e289106b8945c1627440
Oissp <shenspeed@gmail.com>
2026-03-21 19:47:19 +0800

    fix: use jsdelivr CDN for jQuery (more reliable)

--- 115RenamePlus.user.js
+++ 115RenamePlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                115RenamePlus
 // @namespace           https://github.com/Oissp/115RenamePlus/
-// @version             0.10.4
+// @version             0.10.5
 // @updateURL           https://raw.githubusercontent.com/Oissp/115RenamePlus/master/115RenamePlus.user.js
 // @downloadURL         https://raw.githubusercontent.com/Oissp/115RenamePlus/master/115RenamePlus.user.js
 // @description         115RenamePlus(根据现有的文件名<番号>查询并修改文件名)
@@ -14,7 +14,7 @@
 // @domain              seedmm.blog
 // @domain              adult.contents.fc2.com
 // @domain              javdb.com
-// @require             https://lib.sinaapp.com/js/jquery/3.6.0/jquery.min.js
+// @require             https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
 // @grant               GM_notification
 // @grant               GM_xmlhttpRequest
 // ==/UserScript==
